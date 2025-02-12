981ecd735451f7e65a5227e02510f05e3a578c71
kirshiv <rainakirtan27@gmail.com>
2025-02-12 15:29:20 +0530

    build dependencies

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ import cors from "cors"
 import mediaRoute from "./routes/media_route.js"
 import purchaseRoute from "./routes/purcahse_route.js"
 import lectureRoute from "./routes/courseProgress_route.js"
+import path from "path"
 dotenv.config({});
+
+const __dirname = path.resolve();
 connectDB();
 const app=express();
 const PORT=process.env.PORT;
@@ -24,6 +27,12 @@ app.use("/api/v1/user",userRoute);
 app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/purchase", purchaseRoute);
 app.use("/api/v1/lecture", lectureRoute);
+
+
+app.use(express.static(path.join(__dirname, "/client/LMS/dist")));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"client","LMS","dist","index.html"))
+})
 app.listen(PORT,()=>{
     console.log(`Server is saying hello at ${PORT}`);
 })
